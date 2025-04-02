f2a6858a3a5fb0b3bfdfb7f2ad184c86ec8304ab
Gaspard-Isen <145045112+Gaspard-Isen@users.noreply.github.com>
2025-04-02 12:02:43 +0200

    BDD except routes

--- src/model/BDD.cpp
+++ src/model/BDD.cpp
@@ -1,6 +1,6 @@
 ﻿#include "BDD.hpp"
 
-BDD::BDD ( std::string host, std::string nomBDD, std::string login, std::string pwd) {
+BDD::BDD (std::string host, std::string nomBDD, std::string login, std::string pwd) {
 		/* Create a connection */
 		sql::Driver *driver = get_driver_instance();
 		con = driver->connect(host, login, pwd);
@@ -9,25 +9,23 @@ BDD::BDD ( std::string host, std::string nomBDD, std::string login, std::string 
 		con->setSchema(nomBDD);
 		setlocale(LC_ALL,"C");
 
-		Contour contour(contour);
+
+		std::vector<Point> points;
+		Contour contour(points);
 		std::vector<Waypoint> waypoints;
 		std::vector<Route> routes;
 		std::vector<Ville> villes;
 
-
-		this->carte = Carte(contour,routes,villes,waypoints);
-
-		
-
-		
-		/*readContourFromDb(contour);
+		readContourFromDb(contour);
 		readWaypointsFromDb(waypoints);
 		readVilleFromDb(villes);
-		readRouteFromDb(routes);
 
 
+		this->carte = Carte(contour,routes,villes,waypoints);
+	
 
-		this->carte = Carte(contour, routes, villes, waypoints);*/
+		//readRouteFromDb(routes);
+
 }
 
 BDD::~BDD(){
@@ -35,8 +33,9 @@ BDD::~BDD(){
 	delete con;
 }
 
-/*void BDD::readContourFromDb(Contour &Contour){
+void BDD::readContourFromDb(Contour &contour){
 	
+	std::vector<Point> points;
 	sql::Statement *stmt = con->createStatement();
 	sql::ResultSet *res = stmt->executeQuery("SELECT num_pt, lat, lon FROM contour");
 	
@@ -45,37 +44,13 @@ BDD::~BDD(){
 		int num_pt = res->getInt("num_pt");
 		float lat = res->getDouble("lat");
 		float lon = res->getDouble("lon");
-		Contour.getPoints().push_back(Point(num_pt,lat,lon));
+		points.push_back(Point(num_pt,lat,lon));
 	}
+    contour = Contour(points);
+
+
 };
 
-void BDD::readRouteFromDb(std::vector<Route> & route){
-	
-	sql::Statement *stmt = con->createStatement();
-	sql::ResultSet *res = stmt->executeQuery("SELECT nom_debut, nom_fin, distance FROM route");
-	
-
-	while(res->next()){ 
-		std:: string num_debut = res->getString("nom_debut");
-		std:: string nom_fin = res->getString("nom_fin");
-		int distance = res->getInt("distance");
-		route.push_back(Route(num_debut,nom_fin,distance));
-	}
-};
-
-void BDD::readVilleFromDb(std::vector<Ville> & ville){
-	
-	sql::Statement *stmt = con->createStatement();
-	sql::ResultSet *res = stmt->executeQuery("SELECT code_postal, nb_habitants, site FROM ville");
-	
-
-	while(res->next()){ 
-		std::string code_postal = res->getString("code_postal");
-		int nb_habitants = res->getInt("nb_habitants");
-		std:: string site = res->getString("site");
-		ville.push_back(Ville(code_postal,nb_habitants,site));
-	}
-};
 
 void BDD::readWaypointsFromDb(std::vector<Waypoint> & waypoint){
 	
@@ -89,5 +64,36 @@ void BDD::readWaypointsFromDb(std::vector<Waypoint> & waypoint){
 		int lon = res->getInt("lon");
 		waypoint.push_back(Waypoint(nom,lat,lon));
 	}
+};
+
+void BDD::readVilleFromDb(std::vector<Ville> & ville){
+	
+	sql::Statement *stmt = con->createStatement();
+	sql::ResultSet *res = stmt->executeQuery("SELECT nom, code_postal, nb_habitants, site FROM ville");
+
+	while(res->next()){ 
+		std::string nom = res->getString("nom");
+		std::string code_postal = res->getString("code_postal");
+		int nb_habitants = res->getInt("nb_habitants");
+		std:: string site = res->getString("site");
+		ville.push_back(Ville(nom,code_postal,site,nb_habitants));
+	}
+};
+
+/*void BDD::readRouteFromDb(std::vector<Route> & route){
+	
+	sql::Statement *stmt = con->createStatement();
+	sql::ResultSet *res = stmt->executeQuery("SELECT nom_debut, nom_fin, distance FROM route");
+	
+
+	while(res->next()){ 
+		std:: string num_debut = res->getString("nom_debut");
+		std:: string nom_fin = res->getString("nom_fin");
+		int distance = res->getInt("distance");
+		route.push_back(Route(num_debut,nom_fin,distance));
+	}
 };*/
+
+
+
 
--- src/model/Ville.hpp
+++ src/model/Ville.hpp
@@ -8,10 +8,13 @@
 #include <sstream>
 
 class Ville : public Waypoint {
+    private:
+       std::string code_postal;
+       std::string site;
+       int nb_habitants;       
     public:
        // Constructor
-        Ville(std::string nom, float lon, float lat, std::string code_postal_init, int nb_habitants_init, std::string site_init)
-        : Waypoint(nom, lon, lat), code_postal(code_postal_init), nb_habitants(nb_habitants_init), site(site_init) {}
+        Ville(std::string nom, std::string code_postal, std::string site, int nb_habitants): Waypoint(nom), code_postal(code_postal), site(site),  nb_habitants(nb_habitants) {};
 
        
        // Destructor
@@ -22,17 +25,14 @@ class Ville : public Waypoint {
 
         std::string getInfos() const {
             std::ostringstream texte;
-            texte << "Site: " << site << " | Code postal: " << code_postal << " | Nb habitants: " << nb_habitants;
+            texte <<  "Site: " << site << " | Code postal: " << code_postal << " | Nb habitants: " << nb_habitants;
             return texte.str();
         }
 
         // Display relevent informations about the 'ville'
         void affiche() const {std::cout << "\tCode postal: " << code_postal << "\n\t Site: " << site << "\n\t nb_habitants: " << nb_habitants << "\n";}
        
-    private:
-       std::string code_postal;
-       std::string site;
-       int nb_habitants;
+    
 
 }; 
 
