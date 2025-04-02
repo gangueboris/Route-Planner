8d7c772ee31cc9f6564c62e1acb2e7c6fd5f7d38
Gaspard-Isen <145045112+Gaspard-Isen@users.noreply.github.com>
2025-04-02 10:33:04 +0200

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/*===Include of project===*/
+#include "model/BDD.hpp"
+#include "model/Carte.hpp"
+#include "model/Contour.hpp"
+#include "model/Graph.hpp"
+#include "model/Point.hpp"
+#include "model/Route.hpp"
+#include "model/Ville.hpp"
+#include "model/Waypoint.hpp"
+
+int main (){
+    std::string host, base, user, pwd;
+    host = "localhost";
+    base = "routeplanner";
+    user = "routePlanner";
+    pwd = "azerty1234";
+    try {
+        BDD BDD("tcp://"+host+":3306",base,user,pwd);
+    }
+    catch (sql::SQLException &e){
+        std::cout << "Erreur MYSQL, sortie du programme\n";
+        return 1;
+    }
+
+
+    return 0;
+}
